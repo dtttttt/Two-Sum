ef864d0d5f8df2091977fee0b0d52ce5c0b5d308
dtttttt <47856979+dtttttt@users.noreply.github.com>
2019-02-22 11:09:43 +0800

    3th
    先把指定数组进行sort排序，然后用两边向中间夹逼的方式找
    8 ms	9.6 MB

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,20 +1,42 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> result;
-	for (int i = 0; i < nums.size()-1;i++)
+//实现功能函数
+//如果频繁用到某一个函数例如vecoter的size（）最好定义一个变量装他，省的每次都调用一遍
+vector<int> twoSum(vector<int>& nums, int target){
+	vector<int> result;
+	vector<int> numbers(nums);
+	sort(numbers.begin(), numbers.end());
+	int left = 0;
+	int length = nums.size();
+	int right = length-1;//*这个地方要减一别忘了
+	int sum = 0;
+    while (left<right)
+    {
+		sum = numbers[left] + numbers[right];
+		if (sum<target)
 		{
-		for (int j = i + 1; j < nums.size(); j++)
+			left++;
+		}
+		else if (sum>target)
+		{
+			right--;
+		} 
+		else
+		{
+			
+			for (int i = 0; i < length; i++)
 			{
-				if (nums[i]+nums[j]==target)
+				if (nums[i]==numbers[left])
 				{
 					result.push_back(i);
-					result.push_back(j);
-                    return result;
-					
-				} 
+				}
+				else if (nums[i] == numbers[right])  //这里写else是为了防止[3,3]，6这种情况出来的会是[0,0,1,1]
+				{
+					result.push_back(i);
+				}
 			}
-		}   
-    return result;
-    }   
-};
+			
+			return result;  //必须要写，否则会一直跳不出来
+		}
+    }
+
+	return result;
+}
