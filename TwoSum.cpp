b7cfd6482f31fda56fe6f671e984ec1097bfd037
dtttttt <47856979+dtttttt@users.noreply.github.com>
2019-02-22 16:53:53 +0800

    4th
    这个效率不是最高，但用到了map的思想，及先把数组值和坐标映射一下，然后线性遍历，用目标减这个值，看映射中有无差值的存在，来找另外一个数位置

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,42 +1,24 @@
 //实现功能函数
 //如果频繁用到某一个函数例如vecoter的size（）最好定义一个变量装他，省的每次都调用一遍
 vector<int> twoSum(vector<int>& nums, int target){
-	vector<int> result;
-	vector<int> numbers(nums);
-	sort(numbers.begin(), numbers.end());
-	int left = 0;
+	unordered_map<int, int> m; ///和map大致一样，只是这个是无序的，若不要求排序这个效率更高
+	vector<int> res;
 	int length = nums.size();
-	int right = length-1;//*这个地方要减一别忘了
-	int sum = 0;
-    while (left<right)
-    {
-		sum = numbers[left] + numbers[right];
-		if (sum<target)
-		{
-			left++;
-		}
-		else if (sum>target)
-		{
-			right--;
-		} 
-		else
-		{
-			
-			for (int i = 0; i < length; i++)
-			{
-				if (nums[i]==numbers[left])
-				{
-					result.push_back(i);
-				}
-				else if (nums[i] == numbers[right])  //这里写else是为了防止[3,3]，6这种情况出来的会是[0,0,1,1]
-				{
-					result.push_back(i);
-				}
-			}
-			
-			return result;  //必须要写，否则会一直跳不出来
-		}
-    }
+	for (int  i = 0; i < length; i++)
+	{
+		m[nums[i]] = i;
+		//m[i] = nums[i];//这样是不对的
 
-	return result;
-}
+	}
+	for (int i = 0; i < length;i++)
+	{
+		int result = target - nums[i];
+		if (m.count(result)&&m[result]!=i)
+		{
+			res.push_back(i);
+			res.push_back(m[result]);
+			return res;
+		}
+	}
+	return res;
+		}
